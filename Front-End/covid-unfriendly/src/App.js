b25771f559a0ae475796d3e3a0339be10bec5e06
Mekouar Ayoub <ayoubmekouar@gmail.com>
2021-02-15 16:45:33 +0100

    Creating router
    with dependencies (some are for other components)

--- Front-End/covid-unfriendly/src/App.js
+++ Front-End/covid-unfriendly/src/App.js
@@ -1,24 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import MapFrance from './component/MapFrance'
+import data from "./d3js/region.json"
+import { Tooltip } from "redux-tooltip"
+import {useState} from "react"
+import {DepartementDataTab} from "./components/dep-vis/departement-data-tab";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+        <div id="map">
+          <h1>Bienvenue sur covid unfriendly</h1>
+          <Router>
+            <Switch>
+            <Route exact path="/"> 
+              <MapFrance data={data}/>
+            </Route>
+            <Route exact path="/regions">
+
+            </Route>
+            <Route exact path="/departements">
+              <DepartementDataTab />
+            </Route>
+            </Switch>
+          </Router>
+        </div>
+    </body>
   );
 }
 
