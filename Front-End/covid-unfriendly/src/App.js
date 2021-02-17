2afb952d0d2e1fccceac9514606cac7a4bc0445e
Charles Roger <charles.roger@etu.unice.fr>
2021-02-17 20:11:23 +0100

    #7 theme qui peut être changé avec un bouton et persistant en utilisant window.localStorgae

--- Front-End/covid-unfriendly/src/App.js
+++ Front-End/covid-unfriendly/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+
+import { useDarkMode } from './useDarkMode';
+import { lightTheme, darkTheme } from './theme';
+import { GlobalStyles } from './global';
+
+import Toggle_button_theme from './components/Toggle_button_theme';
 
 function App() {
+  const [theme, toggleTheme, componentMounted] = useDarkMode();
+  const themeMode = theme === 'light' ? lightTheme : darkTheme;
+
+  if (!componentMounted) {
+    return <div />
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={themeMode}>
+      <>
+        <GlobalStyles />
+        <Toggle_button_theme theme={theme} toggleTheme={toggleTheme} />
+      </>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
