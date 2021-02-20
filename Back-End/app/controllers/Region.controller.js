9452650a40a17c00f8f7855e5d083fe3f635eabd
Mekouar Ayoub <ayoubmekouar@gmail.com>
2021-02-20 20:32:26 +0100

    Making controller + creatiing test file
    May not work

--- Back-End/app/controllers/Region.controller.js
+++ Back-End/app/controllers/Region.controller.js
@@ -3,14 +3,14 @@ const Jour = db.Jour;
 const Region = db.Region;
 
 // Retrieve all Region data from the database by number and date.
-exports.findAllByRegionNum = (req, res) => {
+exports.findAllByRegionNum = async (req, res) => {
     const date = req.query.date;
     const region_num=req.query.region_num;
     var condition_date = date ? { date: { $regex: new RegExp(date), $options: "i" } } : {};
     var condition_region = region_num ? { region_num: { $regex: new RegExp(region_num), $options: "i" } } : {};
     if( condition_region !== undefined ){
       if( condition_date !== undefined ){
-      Region.find(condition_region,condition_date)
+      await Region.find(condition_region,condition_date)
     .then(data => {
       res.send(data); 
     })
@@ -36,14 +36,14 @@ exports.findAllByRegionNum = (req, res) => {
     
 
 // Retrieve all Region data from the database by libelle and date.
-  exports.findAllByRegionLibelle = (req, res) => {
+  exports.findAllByRegionLibelle = async (req, res) => {
     const date = req.query.date;
     const region_lib=req.query.region_num;
     var condition_date = date ? { date: { $regex: new RegExp(date), $options: "i" } } : {};
     var condition_region = region_lib ? { region_libelle: { $regex: new RegExp(region_lib), $options: "i" } } : {};
     if( condition_region !== undefined ){
       if( condition_date !== undefined ){
-      Region.find(condition_region,condition_date)
+      await Region.find(condition_region,condition_date)
     .then(data => {
       res.send(data); 
     })
@@ -68,11 +68,11 @@ exports.findAllByRegionNum = (req, res) => {
 };
 
 // Find a single Region at date
-  exports.findOne = (req, res) => {
+  exports.findOne = async (req, res) => {
       
     };
 
 // Find all Region for age class at date
-exports.findAllForClasse = (req, res) => {
+  exports.findAllForClasse = async (req, res) => {
     
   };
