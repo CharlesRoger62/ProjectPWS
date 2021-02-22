c0f247e2ba3d1ce37818b458024045d9e12cc014
Thomas <thomas.colomban@etu.unice.fr>
2021-02-22 21:02:35 +0100

    Adding route for picking up last data from departement

--- Back-End/app/api/departements/index.js
+++ Back-End/app/api/departements/index.js
@@ -1,13 +1,14 @@
 /* eslint-disable linebreak-style */
 const express = require('express');
 
-const router = express.Router();
-const departementController = require('../../controllers/Departement.controller');
+router.get('/departements/', departement_controller.findAll);
 
 router.get('/departements', departementController.findAll);
 
 router.get('/departement/bynum/:departementNum', departementController.findAllWithId);
 
-router.get('/departement/bylib/:departementLibelle', departementController.findAllWithLabel);
+router.get('/departement/lastdata/:departement_libelle',departement_controller.findLastDataWithLabel)
 
 module.exports = router;
+
+//http://localhost:9428/api/servicedepartements/departement/bylib/lastdata/
