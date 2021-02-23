3733dd23a0dc9b27d6583a11ffcdb47ad0c0dd6b
Mekouar Ayoub <ayoubmekouar@gmail.com>
2021-02-23 19:42:13 +0100

    reorganizing test code

--- Back-End/app/test/departement/index.js
+++ Back-End/app/test/departement/index.js
@@ -1,36 +1,36 @@
-const assert = require( "chai" ).assert;
-const mocha = require( "mocha" );
-const DepartementService = require( "../../services/departement.service" );
+/* eslint-disable linebreak-style */
+const { assert } = require('chai');
+const mocha = require('mocha');
+const DepartementService = require('../../services/departement.service');
 
-mocha.describe( "Departements Service", () => {
-  
-  mocha.describe( "Departement find All", () => {
-     it( "Is not null ", () => {
-       assert.isNotNull( DepartementService.findAll);
-     } );
-   
-     it( "Is object", () => {
-        assert.isObject( DepartementService.findAll);
-     } );
-   } );
+mocha.describe('Departements Service', () => {
+  mocha.describe('Departement find All', () => {
+    it('Is not null ', () => {
+      assert.isNotNull(DepartementService.findAll);
+    });
 
-   mocha.describe( "Departement find All with Id", () => {
-    it( "Is not null ", () => {
-      assert.isNotNull( DepartementService.findAllWithId);
-    } );
-  
-    it( "Is object", () => {
-       assert.isObject( DepartementService.findAllWithId);
-    } );
-  } );
+    it('Is object', () => {
+      assert.isObject(DepartementService.findAll);
+    });
+  });
 
-  mocha.describe( "Departement find All with Label", () => {
-    it( "Is not null ", () => {
-      assert.isNotNull( DepartementService.findAllWithLabel);
-    } );
-  
-    it( "Is object", () => {
-       assert.isObject( DepartementService.findAllWithLabel);
-    } );
-  } );
-} );
+  mocha.describe('Departement find All with Id', () => {
+    it('Is not null ', () => {
+      assert.isNotNull(DepartementService.findAllWithId);
+    });
+
+    it('Is object', () => {
+      assert.isObject(DepartementService.findAllWithId);
+    });
+  });
+
+  mocha.describe('Departement find All with Label', () => {
+    it('Is not null ', () => {
+      assert.isNotNull(DepartementService.findAllWithLabel);
+    });
+
+    it('Is object', () => {
+      assert.isObject(DepartementService.findAllWithLabel);
+    });
+  });
+});
