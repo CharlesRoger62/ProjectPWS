258cac5362b3b152666447eb94c57bec8a00fadd
Thomas <thomas.colomban@etu.unice.fr>
2021-02-24 18:52:02 +0100

    Adding Location

--- Front-End/covid-unfriendly/src/components/state-vis/Map.jsx
+++ Front-End/covid-unfriendly/src/components/state-vis/Map.jsx
@@ -1,16 +1,17 @@
-import React, {useRef, useEffect, useState} from "react";
-import {select, geoPath, geoMercator,geoConicConformal } from "d3";
+import React, {useRef, useEffect, useState,useMemo} from "react";
+import {select, geoPath,geoConicConformal } from "d3";
 import france from "../../d3js/region.json"
+import {DepartementLastDataLoader} from "../../api/DepartementDataLoader"
+import {RegionLastDataLoader} from "../../api/RegionDataLoader"
+import RegionEnum from '../../enum/RegionEnum'
 import {
    BrowserRouter as Router,
-   Switch,
-   Route,
-   Link,
    useLocation,
    useHistory
  } from "react-router-dom";
 
-function GeoChart(){
+function GeoChart({localisation}){
+   
    const svgRef = useRef();
    const wrapperRef = useRef();
    let history = useHistory();
@@ -21,6 +22,9 @@ function GeoChart(){
 
    const [textTooltip, setTextTooltip] = useState("");
 
+
+   const [covidDataDictionnary, setCovidDataDictionnary] = useState();
+
    let styleTooltip = {
         container: {
             opacity: opacity,
@@ -28,20 +32,39 @@ function GeoChart(){
             left: left
         }
    }
+   
 
-   let location = useLocation();    
-   let data = null
-   if(location.pathname === '/'){
-      data = france
-   }
-   else{
-      const regionFolder = require.context('../../d3js/RegionsMap',true);
-      data = regionFolder('./'+location.state.regionName+'.json');
-   }
+   let location = useLocation();   
+   const regionFolder = require.context('../../d3js/RegionsMap',true); 
+   let data = location.pathname === '/' ? france : regionFolder(`./${location.state.regionName}.json`); 
+   let covidData = null;
+   
+   console.warn("CA PASSSSSE")
+   
+   useEffect(()=> {
+      let covidDataDictionnary = {};
+      if( location.pathname === '/'){
+         RegionLastDataLoader().then( res => {
+            covidData = res.data;
+            setCovidDataDictionnary(Object.assign({}, ...covidData.map((x) => ({[x.region_num]: x}))))
+           })
+      }
+      else {
+        let region_number = RegionEnum[location.state.regionName];
+        DepartementLastDataLoader(region_number).then( res => {
+           covidData = res.data;
+           covidDataDictionnary = Object.assign({}, ...covidData.map((x) => ({[x.departement_num]: x})))
+        })
+      }
+   },[covidData])
 
-   useEffect(() => {
-
+   useEffect((covidDataDictionnary) => {
       const svg = select(svgRef.current);
+
+      if(location.pathname === '/'){
+         svg.selectAll("g").remove()
+      }
+
       const projection = geoConicConformal()
       .fitSize([500,500],data)
 
@@ -57,16 +80,23 @@ function GeoChart(){
             if(location.pathname === '/'){
                setOpacity(0);
                setTextTooltip("");
-
-               history.push({
-                  pathname: '/regions',
-                  state: {regionName : d.target.__data__.properties.nom}
-                  });
+               svg.selectAll("g").remove()
             }
+
+            history.push({
+               pathname: '/regions',
+               state: {regionName : d.target.__data__.properties.nom}
+            });
          })
          .on("mouseover", function(d) {
+            if(location.pathname === '/regions'){
+               setTextTooltip(`Département : ${d.target.__data__.properties.nom} '\n' Nombre de cas positifs : ${covidDataDictionnary[d.target.__data__.properties.code].nbtest_positif}`);
+            }
+            else{
+               console.warn(covidDataDictionnary[RegionEnum[d.target.__data__.properties.nom]].nbtest_positif)
+               setTextTooltip(`Région : ${d.target.__data__.properties.nom} '\n' Nombre de cas positifs : ${covidDataDictionnary[RegionEnum[d.target.__data__.properties.nom]].nbtest_positif}`);
+            }
             setOpacity(0.9);
-            setTextTooltip("Région : " + d.target.__data__.properties.nom);
             var x = d.clientX;
             var y = d.clientY;
             setLeft(x+20);
@@ -76,13 +106,26 @@ function GeoChart(){
             setOpacity(0);
             setTextTooltip("");  
         });
-   } , [history,data,location]);
+
+        if(localisation !== undefined && localisation!== null){
+            svg.selectAll("g")
+            .data(data.features)
+            .enter()
+               .append("g")
+               .style("fill","red")
+               .attr("transform", function(d) { return "translate(" + projection([localisation[1],localisation[0]]) + ")"; })
+               .append("circle") 
+               .attr("r", 5)
+        }
+   } , [history,data,location,localisation]);
 
 
    return (
       <Router>
          <div ref={wrapperRef} style={{marginBottom: "2rem"}}>
-            <svg ref={svgRef}></svg>
+            <svg ref={svgRef}>
+               <g></g>
+            </svg>
             <div class="tooltip" style={styleTooltip.container}>{textTooltip}</div>
          </div>
       </Router>
