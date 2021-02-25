faf263c4626cbe08eaa91e908a61f3f4f24e2204
Ayoub555555 <ayoubmekouar@gmail.com>
2021-02-25 12:29:39 +0100

    adding specific route for sending mail

---
+++ Back-End/app/api/Protected Route/index.js
@@ -0,0 +1,35 @@
+/* eslint-disable linebreak-style */
+const express = require('express');
+
+const router = express.Router();
+const session = require('express-session');
+
+router.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  unset: 'destroy',
+  saveUninitialized: true,
+  cookie: {
+    secure: false,
+    user: 'user',
+  },
+}));
+
+const requireRole = (role) => function requester(req, res, next) {
+  if (req.session.cookie.user === role) {
+    next();
+  } else {
+    res.send(403);
+  }
+};
+
+router.get('/getpassword/:cryptedpassword', requireRole('user'), (req, res) => {
+  try {
+    console.log('ok');
+    res.send(`${req.params.cryptedpassword}4`);
+  } catch (e) {
+    console.log(e.message);
+  }
+});
+
+module.exports = router;
