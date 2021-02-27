d93bb174d25a48196c05533f64d499e44a6c71d2
Rayan Benmammar <rayan.benmammar90@gmail.com>
2021-02-27 17:43:10 +0100

    get the news of the day before from api

--- Front-End/covid-unfriendly/src/components/header/header.js
+++ Front-End/covid-unfriendly/src/components/header/header.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Navbar, Nav} from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.css';
 import covidLogo from '../../assets/images/covid.png'
 import polytechLogo from '../../assets/images/polytech.png'
+import Spinner from 'react-bootstrap/Spinner'
 
 export const HeaderNav =  () => {
+
+    const [data, setData] = useState({});
+    const [loading, setLoading] = useState(true);
+    const url = 'https://coronavirusapi-france.now.sh/FranceLiveGlobalData';
+
+    const fetchData = async () => {
+        const response = await fetch(url);
+        const json = await response.json();
+        setData(json.FranceGlobalLiveData[0]);
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
 
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
@@ -21,6 +37,9 @@ export const HeaderNav =  () => {
                     width="120"
                     height="50"
                 />
+                {loading
+                    ?<Spinner animation="border" variant="danger" />
+                    :<p>{data.deces} décès pour {data.gueris } guéris ({data.date})</p> }
             </Navbar.Brand>
         </Navbar>
     );
